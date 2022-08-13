6736151fa9547e1f55d4fec9e35f98ca9a1880b9
Camilo Hernández Ruiz <camihruiz24@gmail.com>
2022-08-13 17:08:52 -0500

    GW_info

--- telenet.sh
+++ telenet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 opcion=$(whiptail --title 'Menu Telnet' --fb \
---menu "      ===================== \n              TELNET \n             ====================== \n Por Camilo Hernández Ruiz \n camilo.hernandez1@udea.edu.co \n Servicios Telemáticos \n Ingneiería de Telecomunicaciones \n Universidad de Antioquia \n Semestre 2022 \n " \
+--menu "      ===================== \n              TELNET \n             ====================== \n Por Camilo Hernández Ruiz \n camilo.hernandez1@udea.edu.co \n Servicios Telemáticos \n Ingeniería de Telecomunicaciones \n Universidad de Antioquia \n Semestre 2022-1 \n " \
 30 40 5 \
 "1" "Información de red" \
 "2" "Consulta DNS" \
@@ -13,6 +13,7 @@ case $opcion in
     1)
         ip=$(ifconfig enp0s3 | awk 'NR==2 {print $2}')
         mask=$(ifconfig enp0s3 | awk 'NR==2 {print $4}')
+        gw=$(ip r | awk 'NR==1 {print$3}')
         
-        whiptail --title "Opcion 1: Información de red" --msgbox "IP: $ip\nMASK: $mask" 20 40 2
+        whiptail --title "Opcion 1: Información de red" --msgbox "IP: $ip\nMASK: $mask\nGW: $gw" 20 40 2
 esac
